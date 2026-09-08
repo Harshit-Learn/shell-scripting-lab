d3d05414e7b9cb0c3d1de277ca2c537563df915c
Harshit-Learn <harshitbaranwal609@gmail.com>
2026-09-08 06:29:25 +0000

    Shell script

--- day02/basic_scripting_skills.sh
+++ day02/basic_scripting_skills.sh
@@ -27,7 +27,7 @@ B) To execute the script , these are two method
 
 comment
 
-
+<<comment
 # 1.Variables
 name="gjfdjfhjfh"
 echo "Name is $name , Date is $(date)"
@@ -37,18 +37,65 @@ echo "Enter The name:"
 read username 
 echo "Your entered $username"
 
-# 3. Second way to take user input ( p = prompt dena)
+# 2. Second way to take user input ( p = prompt dena)
 read -p "Enter UserName" username
 echo "Your entered $username"
 
 
 # Example of taking Input from client
-sudo useradd -m $username
+#sudo useradd -m $username
 echo "New user added"
 
 
-# Auguments
-# harshit@DESKTOP-JG6Q88N:~/shell-scripting-lab/day02$ argument0($0) argument1($1) argument2($2)
+## 3.Argument
+<<comment 
+What is  Auguments
+Arguments are values passed to a script when running it.
+$0 → Script ( File Name)
+$1 → First
+$2 → Second
+$3 → Third
+$# → Count
+$@ → All
+$? → Status
+
+echo "Script/file name: $0"
+echo "First: $1"
+echo "Second: $2"
+echo "Total arguments: $#"
+echo "All arguments: $@"
+
+
+# Conditions(if , else , elif)
+
+age=20
+
+if [ $age -ge 13 ]; 
+then
+  
+    	echo "You are eligible for Vote"
+else
+	echo "Not eligible"
+fi
+comment
+
+read -p "Enter your Name" name;
+
+if [ $name == Harshit ];
+then 
+          echo"This is correct name"
+elif [ $name == "raju" ];
+then
+	  echo"This is Raju name"
+
+else
+	echo"This is wrong name"
+fi;
+
+
+
+
+
 
 
 
